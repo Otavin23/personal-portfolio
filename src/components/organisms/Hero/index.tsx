259afc0769869f9a72b94520cfd41b93e0001e45
Otavin23 <alvesotavio132@gmail.com>
2023-12-24 10:23:43 -0300

    :lipstick: style: put download icon on the download resume button in the hero section

--- src/components/organisms/Hero/index.tsx
+++ src/components/organisms/Hero/index.tsx
@@ -96,6 +96,24 @@ const Hero = () => {
             mr={{ sm: "1rem", base: "0" }}
           >
             Baixar Curriculo
+            <svg
+              style={{
+                marginLeft: "0.5rem",
+              }}
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+              <polyline points="7 10 12 15 17 10" />
+              <line x1="12" x2="12" y1="15" y2="3" />
+            </svg>
           </Button>
 
           <Button
